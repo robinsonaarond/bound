05f8d878003328e7b079846a6c4cccc91e792826
bl4ckmesa <robinsonaarond@gmail.com>
2013-11-30 23:54:27 -0800

    Almost a complete rewrite; using the b2a_hex function now, and then converting the domain stuff back to ascii for printing out.  Now all the word lengths are exactly accurate, so I could break them up dependably using positional data.  Shortened code by using single split with regex to get all values.

--- bound.py
+++ bound.py
@@ -2,6 +2,7 @@
 
 import socket,select
 import binascii
+import re
 
 UDP_IP = '0.0.0.0'
 UDP_PORT = 10053
@@ -9,96 +10,55 @@ UDP_PORT = 10053
 # message should be base64 encoded
 def parse_query(message):
 
-	# Mostly...
-	# =E9=94=01=00=00=01=00=00=00=00=00=00=03www=06google=03com=00=00=01=00=01
-	# Every once in a while...
-	# =A1X=01=00=00=01=00=00=00=00=00=00=03www=06google=03com=00=00=01=00=01
-	# Or...
-	# =CFU=01=00=00=01=00=00=00=00=00=00=03www=06google=03com=00=00=01=00=01
-	# Or...
-	# M=A0=01=00=00=01=00=00=00=00=00=00=03www=06google=03com=00=00=01=00=01
-	# Or...
-	# :=18=01=00=00=01=00=00=00=00=00=00=03www=06google=03com=00=00=01=00=01
-	# Or...
-	# <?=01=00=00=01=00=00=00=00=00=00=03www=06google=03com=00=00=01=00=01
-
-	wlength = 6
-	nchar = 0
-	# First 4 ID, e.g., =E9=94 or =A1X or :=18
-	# Basically, I'm punting and will use the message_id, which is always the
-	# same, to find the id.
-	message_id = message[nchar:message.find("=01=00")]
-	#message_id = message[nchar:nchar+wlength].replace("=", "")
-	print "Message ID:", message_id
-	nchar += len(message_id)
-	# Next 4 Flags e.g., =01=00
-	message_flags = message[nchar:nchar+wlength].replace("=", "")
-	print "Message Flags:", message_flags
-	nchar += wlength
-	# Next 4 QDCOUNT e.g., =00=01
-	message_qdcount = message[nchar:nchar+wlength].replace("=", "")
-	print "Message QDCOUNT:", message_qdcount
-	nchar += wlength
-	# Next 4 ANCOUNT e.g., =00=00
-	message_ancount = message[nchar:nchar+wlength].replace("=", "")
-	print "Message ANCOUNT:", message_ancount
-	nchar += wlength
-	# Next 4 NSCOUNT e.g., =00=00
-	message_nscount = message[nchar:nchar+wlength].replace("=", "")
-	print "Message NSCOUNT:", message_nscount
-	nchar += wlength
-	# Next 4 ARCOUNT e.g., =00=00
-	message_arcount = message[nchar:nchar+wlength].replace("=", "")
-	print "Message ARCOUNT:", message_arcount
+	# hex values for www.google.edu nslookup
+	# ab5a010000010000000000000377777706676f6f676c65036564750000010001
+	# ID [ab5a], Flags [0100], QDCOUNT [0001], ANCOUNT [0000], NSCOUNT [0000], ARCOUNT [0000]
+	msg_id, msg_flags, msg_qdcount, msg_ancount, msg_nscount, msg_arcount = re.findall('....', message[0:24])
 	domains = []
-	message_data = message[nchar+wlength:]
-	print "Message Data:", message_data
-	for i in range(0,len(message_data)):
-		if message_data[i] == "=":
-			dlength = int(message_data[i+2:i+3])
-			print "Message Dlength:", dlength
-			if dlength != 0:
-				domains.append(message_data[i+3:i+dlength+3])
-				i += dlength
-			else:
-				print "Message Domain:", '.'.join(domains)
-				# Done going through domains segments, now get final bits
-				i += 3
-				# Next 4 Data Query type e.g., =00=01
-				message_querytype = message_data[i:i+6].replace("=", "")
-				print "Message QType:", message_querytype
-				i += 6
-				# Last 4 Data Class type e.g., =00=01
-				message_dataclass = message_data[i:i+6].replace("=", "")
-				print "Message DCType:", message_dataclass
-				# Now clear out, we're done parsing
-				break
-	print "Message Full:", message
+	msg_data = message[24:]
+	i = 0
+	while i < len(msg_data):
+		byte_width = 2
+		# Denotes how long the domain string length will be, in hex. 
+		# Convert to int, multiply by byte char width (usually 2)
+		domain_length = int(msg_data[i:i+byte_width], 16) * byte_width
+		if domain_length != 0:
+			d = msg_data[i+byte_width:i+domain_length+byte_width]
+			dstring = binascii.unhexlify(d)
+			domains.append(dstring)
+			i += domain_length + byte_width
+		else:
+			print "Message Domain:", '.'.join(domains)
+			# Done going through domains segments, now get final bits
+			msg_querytype, msg_dataclass = re.findall('....', msg_data[i+2:])
+			print "Query Type:", msg_querytype
+			print "Data Class:", msg_dataclass
+			#i += 2
+			## Next 4 Data Query type e.g., 0001
+			#message_querytype = msg_data[i:i+4]
+			#i += 4
+			## Last 4 Data Class type e.g., 0001
+			#msg_dataclass = msg_data[i:i+4]
+			## Now clear out, we're done parsing
+			break
 	return True
-
-
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 sock2 = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP)
 sock2.bind((UDP_IP, UDP_PORT))
 
+print "Listening on port %s..." % UDP_PORT
+
 while True:
     r, w, x = select.select([sock, sock2], [], [])
     for i in r:
-		#message = parse_message(i.recvfrom(1024)[0])
-		#ip = packet[1][0]
-		#port = packet[1][1]
-		#('\x9ae\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x03www\x06google\x03com\x00\x00\x01\x00\x01', ('127.0.0.1', 32944))
-
-		message = binascii.b2a_qp(i.recv(1024))
-		hostdata = i.recvfrom(1024)[1] # Having the second recv causes weird hangups
-		print message, hostdata
-	
-		ip = hostdata[0]
-		port = hostdata[1]
-		domain = parse_query(message)
+		packet = i.recvfrom(1024)
+		message = binascii.b2a_hex(packet[0])
+		ip = packet[1][0]
+		port = packet[1][1]
 
 		if port is not 0: # All those broadcast messages from Bonjour! ;)
-			print ip, port
+			domain = parse_query(message)
+			print "Message from:", ip, port
 
